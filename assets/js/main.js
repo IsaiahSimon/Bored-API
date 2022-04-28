92405c0329350a0efebef6a2fc6f85898017e054
Isaiah Simon <60068759+IsaiahSimon@users.noreply.github.com>
2022-04-28 15:39:54 -0400

    📦 NEW: Add basic functionality
    - Create HTML elements (h1, p, h2, button)
    - Add click event listener on button
    - Fetch API object
    - Update h2 element with the "activity" from the API object

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
+document.querySelector('button').addEventListener('click', getFetch)
 
+function getFetch(){
+  // add user choices later on
+  // const choice = document.querySelector('input').value
+  const url = 'http://www.boredapi.com/api/activity/'
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        let h2 = document.querySelector('h2');
+        h2.innerHTML = data.activity;
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
